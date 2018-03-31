a71f9c159d03852ad8b71078c0697d93e3b37b0d
Garin Richards <garinrk@gmail.com>
2018-03-30 20:32:51 -0600

    Deforming the terrain

--- compute/Assets/Scripts/Life.cs
+++ compute/Assets/Scripts/Life.cs
@@ -29,11 +29,6 @@ public class Life : MonoBehaviour {
         result.enableRandomWrite = true;
         result.Create();
 
-    }
-	
-	// Update is called once per frame
-	void Update () {
-
         kernelID = compute.FindKernel("Life"); //returns an id for this function
 
         compute.SetTexture(kernelID, "pictureInput", photoInput); //send in picture
@@ -53,6 +48,31 @@ public class Life : MonoBehaviour {
 
         photoInput = result;
         mat.mainTexture = photoInput;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //kernelID = compute.FindKernel("Life"); //returns an id for this function
+
+        //compute.SetTexture(kernelID, "pictureInput", photoInput); //send in picture
+        //compute.SetFloat("width", width);
+        //compute.SetFloat("height", height);
+
+        //result = new RenderTexture(width, height, 24);
+        //result.wrapMode = TextureWrapMode.Repeat; //wrap around texture
+        //result.enableRandomWrite = true;
+        //result.filterMode = FilterMode.Point; //texture pixels become blocky up close
+        ////how to render when transformed by 3d things
+        //result.useMipMap = false;
+        //result.Create();
+
+        //compute.SetTexture(kernelID, "Result", result);
+        //compute.Dispatch(kernelID, width / 8, height / 8, 1);
+
+        //photoInput = result;
+        //mat.mainTexture = photoInput;
         
     }
 }
---
+++ compute/Assets/Scripts/TerrainGen.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGen : MonoBehaviour {
+
+
+    #region Unity Serialized Fields
+
+    [SerializeField] private Terrain myTerr;
+    [SerializeField] private Terrain backup;
+    [SerializeField] private Texture2D tex;
+
+    #endregion
+
+    #region Private Fields
+
+    private int width;
+    private int height;
+    float[,] heightData;
+    float[,] backupHeightData;
+
+    #endregion
+
+    #region Unity Lifecycle
+
+    // Use this for initialization
+    void Start () {
+
+        width = myTerr.terrainData.heightmapWidth;
+        height = myTerr.terrainData.heightmapHeight;
+
+        myTerr.heightmapMaximumLOD = 0;
+		
+	}
+
+    private void OnDisable()
+    {
+        int backupWidth = backup.terrainData.heightmapWidth;
+        int backupHeight = backup.terrainData.heightmapHeight;
+
+        backupHeightData = backup.terrainData.GetHeights(0, 0, backupWidth, backupHeight);
+
+        myTerr.terrainData.SetHeights(0, 0, backupHeightData);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        heightData = myTerr.terrainData.GetHeights(0, 0, width, height);
+
+        for(int i = 0; i < width / 2; i++)
+        {
+            for(int j = 0; j < height; j++)
+            {
+                heightData[i, j] = 0.24f;
+            }
+        }
+
+        myTerr.terrainData.SetHeights(0, 0, heightData);
+	}
+
+    #endregion
+}
